137bcd052fd352767e11c233101cd0338ac17843
a.hiryanov <a.hiryanov@octaviangameart.ru>
2022-07-29 14:49:05 +0300

    removed unnecessary old commented code

--- Main.cpp
+++ Main.cpp
@@ -1,217 +1,3 @@
-/*#include "Event_Controller.h"
-#include "Shader.h"
-#include "Camera.h"
-#include "Resource_Loader.h"
-
-#include "Object.h"
-#include "Text_Field.h"
-
-#include "Physical_Model_3D.h"
-#include "Physical_Model_2D.h"
-
-#include "Message_Translator.h"
-
-//#include "Space_Splitter_2D.h"
-#include "Space_Splitter_3D.h"
-
-//#include "Timer.h"
-
-struct On_Button_Pressed_Msg
-{
-	DEFINE_TYPE("obpf");
-	unsigned int btn = 0;
-	On_Button_Pressed_Msg(unsigned int _btn) : btn(_btn) { }
-};
-
-int main()
-{
-	LEti::Event_Controller::init_and_create_window(1200, 800, "Board Game");
-
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_DEPTH_TEST);
-
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_CW);
-
-	LEti::Shader::init_shader("Resources/Shaders/vertex_shader.shader", "Resources/Shaders/fragment_shader.shader");
-	ASSERT(!LEti::Shader::is_valid());
-	LEti::Shader::set_texture_uniform_name("input_texture");
-	LEti::Shader::set_transform_matrix_uniform_name("transform_matrix");
-	LEti::Shader::set_projection_matrix_uniform_name("projection_matrix");
-
-	LEti::Camera::setup_orthographic_matrix();
-
-	LEti::Camera::set_fov_and_max_distance(LEti::Math::HALF_PI, 50.0f);
-	LEti::Camera::set_camera_data({ 0.0f, 2.0f, 2.0f }, { 0.0f, -2.0f, -1.0f });
-
-	LEti::Resource_Loader::init();
-
-	LEti::Resource_Loader::load_object("textures", "Resources/Textures/textures.mdl");
-
-	LEti::Resource_Loader::load_object("red_cube", "Resources/Models/red_cube.mdl");
-	LEti::Object_3D cube;
-	cube.init("red_cube");
-
-//	LEti::global_indicator.init("cube");
-
-	LEti::Resource_Loader::load_object("cube", "Resources/Models/intersection_indicator_3d.mdl");
-
-	///////////////// 3d collision test
-
-	LEti::Resource_Loader::load_object("colliding_object", "Resources/Models/colliding_object.mdl");
-	LEti::Object_3D coll_obj;
-	coll_obj.init("colliding_object");
-
-	LEti::Resource_Loader::load_object("pyramid", "Resources/Models/pyramid.mdl");
-	LEti::Object_3D pyramid;
-	pyramid.init("pyramid");
-
-	LEti::Object_3D pyramid_far;
-	pyramid_far.init("pyramid");
-//	pyramid_far.move(5.0f, 5.0f, 5.0f);
-	pyramid_far.move(0, 0, 0);
-
-	LEti::Space_Splitter_3D::register_object(&pyramid);
-	LEti::Space_Splitter_3D::register_object(&pyramid_far);
-	LEti::Space_Splitter_3D::register_object(&coll_obj);
-
-	glm::mat4x4 move
-	(
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f
-	);
-
-	glm::mat4x4 fake_rotate = glm::rotate(pyramid.get_rotation_angle(), pyramid.get_rotation_axis());
-
-	glm::mat4x4 kostyl_matrix
-	(
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f
-	);
-
-	glm::vec3 point(0.3f, 0.3f, 0.3f);
-
-	LEti::Resource_Loader::load_object("text_field", "Resources/Models/text_field.mdl");
-	LEti::Text_Field intersection_info_block;
-	intersection_info_block.init("text_field");
-
-
-	float triangle_speed = 0.5f;
-
-	bool intersection_detected = false;
-	if(intersection_detected)
-		intersection_info_block.set_text("Intersection detected");
-	else
-		intersection_info_block.set_text("Intersection not detected");
-
-	while (!LEti::Event_Controller::window_should_close())
-	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		LEti::Event_Controller::update();
-
-		if (LEti::Event_Controller::key_was_pressed(GLFW_KEY_TAB))
-			LEti::Camera::toggle_controll(LEti::Camera::get_controllable() ? false : true);
-		LEti::Camera::update(false, true);
-
-		if (LEti::Event_Controller::is_key_down(GLFW_KEY_LEFT))
-		{
-			pyramid.move((triangle_speed * LEti::Event_Controller::get_dt()), 0.0f, 0.0f);
-		}
-		if (LEti::Event_Controller::is_key_down(GLFW_KEY_RIGHT))
-		{
-			pyramid.move(-(triangle_speed * LEti::Event_Controller::get_dt()), 0.0f, 0.0f);
-		}
-		if (LEti::Event_Controller::is_key_down(GLFW_KEY_DOWN))
-		{
-			pyramid.move(0.0f, 0.0f, -(triangle_speed * LEti::Event_Controller::get_dt()));
-		}
-		if (LEti::Event_Controller::is_key_down(GLFW_KEY_UP))
-		{
-			pyramid.move(0.0f, 0.0f, (triangle_speed * LEti::Event_Controller::get_dt()));
-		}
-		if (LEti::Event_Controller::is_key_down(GLFW_KEY_I))
-		{
-			pyramid.move(0.0f, (triangle_speed * LEti::Event_Controller::get_dt()), 0.0f);
-		}
-		if (LEti::Event_Controller::is_key_down(GLFW_KEY_K))
-		{
-			pyramid.move(0.0f, -(triangle_speed * LEti::Event_Controller::get_dt()), 0.0f);
-		}
-		if (LEti::Event_Controller::is_key_down(GLFW_KEY_Q))
-		{
-			pyramid.rotate(LEti::Math::QUARTER_PI * LEti::Event_Controller::get_dt());
-		}
-		if (LEti::Event_Controller::is_key_down(GLFW_KEY_E))
-		{
-			pyramid.rotate(- LEti::Math::QUARTER_PI * LEti::Event_Controller::get_dt());
-		}
-
-		fake_rotate = glm::rotate(pyramid.get_rotation_angle(), pyramid.get_rotation_axis());
-		glm::vec3 pyramid_pos = pyramid.get_pos();
-		move[3][0] = pyramid_pos.x;
-		move[3][1] = pyramid_pos.y;
-		move[3][2] = pyramid_pos.z;
-
-		pyramid_far.update();
-		pyramid.update();
-		coll_obj.update();
-
-		LEti::Space_Splitter_3D::update();
-
-		auto list = LEti::Space_Splitter_3D::get_collisions();
-		auto it = list.begin();
-		while(it != list.end())
-		{
-//			std::cout << it->first << ' ' << it->second << "\n";
-			intersection_info_block.set_text(std::to_string(list.size()).c_str());
-			cube.set_pos(it->collision_data.closest_intersection_point.x, it->collision_data.closest_intersection_point.y, it->collision_data.closest_intersection_point.z);
-			cube.draw();
-			++it;
-		}
-//		std::cout << "\n";
-
-//		LEti::Physical_Model_Interface::Intersection_Data id = pyramid.is_colliding_with_other(coll_obj);
-
-
-//		cube.set_visible(false);
-//		std::string intersection_message;
-//		if(id.type == LEti::Physical_Model_Interface::Intersection_Data::Type::inside)
-//			intersection_message += "PM is fully inside";
-//		else if(id.type == LEti::Physical_Model_Interface::Intersection_Data::Type::partly_outside)
-//		{
-//			intersection_message += "intersection at ";
-//			intersection_message += std::to_string(id.closest_intersection_point.x);
-//			intersection_message += ' ';
-//			intersection_message += std::to_string(id.closest_intersection_point.y);
-//			intersection_message += ' ';
-//			intersection_message += std::to_string(id.closest_intersection_point.z);
-//			cube.set_pos(id.closest_intersection_point.x, id.closest_intersection_point.y, id.closest_intersection_point.z);
-////			cube.set_visible(true);
-//		}
-//		else
-//			intersection_message += "no intersection";
-//		intersection_info_block.set_text(intersection_message.c_str());
-
-		pyramid_far.draw();
-		pyramid.draw();
-		coll_obj.draw();
-
-//		cube.draw();
-
-		intersection_info_block.draw();
-
-		LEti::Event_Controller::swap_buffers();
-	}
-
-	return 0;
-}*/
-
 #include "Event_Controller.h"
 #include "Shader.h"
 #include "Camera.h"
@@ -230,6 +16,7 @@ int main()
 #include "Debug_Drawable_Frame.h"
 
 
+
 #define DT LEti::Event_Controller::get_dt()
 
 struct On_Button_Pressed_Msg
@@ -241,7 +28,7 @@ struct On_Button_Pressed_Msg
 
 int main()
 {
-	LEti::Event_Controller::init_and_create_window(1200, 800, "Board Game");
+	LEti::Event_Controller::init_and_create_window(1200, 800, "Collision Test");
 
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -373,9 +160,9 @@ int main()
 
 	while (!LEti::Event_Controller::window_should_close())
 	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		LEti::Event_Controller::update();
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		if (LEti::Event_Controller::key_was_pressed(GLFW_KEY_TAB))
 			LEti::Camera::toggle_controll(LEti::Camera::get_controllable() ? false : true);
